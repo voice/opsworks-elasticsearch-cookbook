633907dcc2e800a76513245f98eb0bd69c83a40f
bassrock <bassrock@users.noreply.github.com>
2014-05-08 11:18:22 -0700

    Create metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "DABSquared, LLC"
+maintainer_email "daniel_brooks@dabsquared.com"
+license          "All rights reserved"
+description      "Installs elasticsearch plugins and other items"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+name             "opsworks-elasticsearch-cookbook"
